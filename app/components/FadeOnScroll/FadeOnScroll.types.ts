de28f04f73a8dd27ffd2f84607f29a36e24b53ee
Johnathan Guzman <johnathanthedev@gmail.com>
2025-06-13 21:47:10 -0500

    feat: improved loading

---
+++ app/components/FadeOnScroll/FadeOnScroll.types.ts
@@ -0,0 +1,6 @@
+import { ReactNode } from "react";
+
+export interface FadeOnScrollProps {
+    children: ReactNode;
+    key?: string
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 import { portfolioFiles } from "@/lib/portfolio";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import LoadingWithContent from "./components/LoadingWithContent/LoadingWithContent.component";
+import FadeOnScroll from "./components/FadeOnScroll/FadeOnScroll.component";
+import LoadingWithContent from "./components/Loading/LoadingWithContent/LoadingWithContent.component";
 import styles from "./page.module.scss";
 
 export default function Home() {
@@ -13,23 +14,22 @@ export default function Home() {
                     <div className="content-section">
                         <div className={styles.gridGallery}>
                             {portfolioFiles.map((src, index) => (
-                                // <FadeOnScroll key={`Gallery ${index}`}>
-                                <div
-                                    key={index}
-                                    className={styles.imageContainer}
-                                // onClick={() => openFullscreen(index)}
-                                >
-                                    <img
-                                        src={src}
-                                        alt={`Gallery ${index + 1}`}
-                                        className={styles.image}
-                                        loading="lazy"
-                                    />
-                                    <div className={styles.overlay}>
-                                        <FontAwesomeIcon icon={faSearch} className={styles.magnifyingIcon} />
+                                <FadeOnScroll key={`Gallery ${index}`}>
+                                    <div
+                                        key={index}
+                                        className={styles.imageContainer}
+                                    >
+                                        <img
+                                            src={src}
+                                            alt={`Gallery ${index + 1}`}
+                                            className={styles.image}
+                                            loading="lazy"
+                                        />
+                                        <div className={styles.overlay}>
+                                            <FontAwesomeIcon icon={faSearch} className={styles.magnifyingIcon} />
+                                        </div>
                                     </div>
-                                </div>
-                                // </FadeOnScroll>
+                                </FadeOnScroll>
                             ))}
                         </div>
                     </div>
