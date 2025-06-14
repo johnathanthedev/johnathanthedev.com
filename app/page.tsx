4930dbd14cb57e16ba09836540e38181075be65f
Johnathan Guzman <johnathanthedev@gmail.com>
2025-06-14 00:35:48 -0500

    feat: banners

--- app/page.tsx
+++ app/page.tsx
@@ -1,40 +1,37 @@
 import { portfolioFiles } from "@/lib/portfolio";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import FadeOnScroll from "./components/FadeOnScroll/FadeOnScroll.component";
 import LoadingWithContent from "./components/Loading/LoadingWithContent/LoadingWithContent.component";
 import styles from "./page.module.scss";
 
 export default function Home() {
     return (
-        <LoadingWithContent>
-            <div className={styles.page}>
-                <h1>DREAMS.</h1>
-                <div className="content-wrapper">
-                    <div className="content-section">
-                        <div className={styles.gridGallery}>
-                            {portfolioFiles.map((src, index) => (
-                                <FadeOnScroll key={`Gallery ${index}`}>
-                                    <div
-                                        key={index}
-                                        className={styles.imageContainer}
-                                    >
-                                        <img
-                                            src={src}
-                                            alt={`Gallery ${index + 1}`}
-                                            className={styles.image}
-                                            loading="lazy"
-                                        />
-                                        <div className={styles.overlay}>
-                                            <FontAwesomeIcon icon={faSearch} className={styles.magnifyingIcon} />
+        <>
+            <div className={styles.leftBanner}>ABOUT ME</div>
+            <div className={styles.rightBanner}>DREAMS X REALITY</div>
+
+            <LoadingWithContent>
+                <div className={styles.page}>
+                    <h1>DREAMS.</h1>
+                    <div className={styles["content-wrapper"]}>
+                        <div className="content-section">
+                            <div className={styles.gridGallery}>
+                                {portfolioFiles.map((src, index) => (
+                                    <FadeOnScroll key={`Gallery ${index}`}>
+                                        <div className={styles.imageContainer}>
+                                            <img
+                                                src={src}
+                                                alt={`Gallery ${index + 1}`}
+                                                className={styles.image}
+                                                loading="lazy"
+                                            />
                                         </div>
-                                    </div>
-                                </FadeOnScroll>
-                            ))}
+                                    </FadeOnScroll>
+                                ))}
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </LoadingWithContent>
+            </LoadingWithContent>
+        </>
     );
 }
